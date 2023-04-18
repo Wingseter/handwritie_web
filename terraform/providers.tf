9bc7de57e2e3f1bc03bcbae789ddde4de20d4097
kwon <wingseter@gmail.com>
2023-04-18 23:58:38 +0900

    [develop] datasbase, rabbit publish with terraform  add two microservice in kubernetes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,13 @@ provider "azurerm" {
 provider "tls" {
 }
 
+provider "kubernetes" {
+    host                   = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
+}
+
 terraform {
   required_providers {
     azurerm = {
@@ -15,5 +22,10 @@ terraform {
       source  = "hashicorp/tls"
       version = ">= 3.1.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.5.0"
+    }
+
   }
 }
